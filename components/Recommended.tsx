ccd50dfa63081f3cd192c87ec070f64125c0a48d
alex <alexlhliang@gmail.com>
2022-07-05 18:20:29 -0700

    got slider working

--- components/Recommended.tsx
+++ components/Recommended.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef, useLayoutEffect } from 'react';
-import { animated, useTransition, useSpringRef } from 'react-spring';
+import React, { useState, useRef, useLayoutEffect, useEffect } from 'react';
+import { animated, useSpring } from 'react-spring';
 
 import styles from '../styles/Recommended.module.scss';
 import { ProductInterface } from '../types/interfaces';
@@ -14,44 +14,31 @@ export default function Recommended({ products }: Props) {
   const productsCtn = useRef<HTMLDivElement | null>(null);
   const productsCtnWidth = useRef('100vw');
 
-  const [page, setPage] = useState(1);
-  const transitions = useTransition(
-    products.filter((p, i) => {
-      const pageMin = (page - 1) * 3;
-      const pageMax = page * 3;
+  const [page, setPage] = useState(0);
+  const lastPage = page === products.length / 3 + (products.length % 3) ? 1 : 0;
 
-      return i >= pageMin && i < pageMax;
-    }),
-    {
-      from: {
-        transform: `translateX(${productsCtnWidth.current})`,
-      },
-      enter: {
-        transform: 'translateX(0px)',
-      },
-      leave: {
-        transform: `translateX(-${productsCtnWidth.current})`,
-      },
-      exitBeforeEnter: true,
-    }
-  );
+  const [slideProps, slidePropsApi] = useSpring(() => ({
+    offset: 0,
+  }));
+
+  useEffect(() => {
+    slidePropsApi.update(() => ({ offset: 0 - page })).start();
+  }, [page, slidePropsApi]);
 
   function pageUp() {
-    setPage((prev) => prev + 1);
+    setPage((prev) => {
+      if (lastPage) return prev;
+      return prev + 1;
+    });
   }
 
   function pageDown() {
-    setPage((prev) => prev - 1);
+    setPage((prev) => {
+      if (page === 0) return prev;
+      return prev - 1;
+    });
   }
 
-  useLayoutEffect(() => {
-    if (!productsCtn.current) return;
-
-    productsCtnWidth.current = window.getComputedStyle(
-      productsCtn.current
-    ).width;
-    console.log(window.getComputedStyle(productsCtn.current).width);
-  }, [page]);
   return (
     <section className={styles.main}>
       <header>
@@ -61,23 +48,38 @@ export default function Recommended({ products }: Props) {
         <button type="button" onClick={pageDown}>
           <ArrowSvg dir="left" />
         </button>
-        <div ref={productsCtn} className={styles.products_ctn}>
-          {products
-            .reduce<JSX.Element[][]>(
-              (acc, curr, i) => {
-                if (acc[acc.length - 1].length < 3)
-                  acc[acc.length - 1].push(<ProductPreview product={curr} />);
-                else {
-                  acc.push([]);
-                  acc[acc.length - 1].push(<ProductPreview product={curr} />);
-                }
-                return acc;
-              },
-              [[]]
-            )
-            .map((productPreviews, i) => {
-              return <div key={i}>{productPreviews}</div>;
-            })}
+        <div className={styles.slide_frame}>
+          <animated.div
+            className={styles.products_ctn}
+            style={{
+              transform: slideProps.offset.to(
+                (offsetX) => `translateX(${offsetX * (772 + 40)}px)`
+              ),
+              willChange: 'transform',
+            }}
+          >
+            {products
+              // split into sections of 3 products
+              .reduce<JSX.Element[][]>(
+                (acc, curr, i) => {
+                  if (acc[acc.length - 1].length < 3)
+                    acc[acc.length - 1].push(<ProductPreview product={curr} />);
+                  else {
+                    acc.push([]);
+                    acc[acc.length - 1].push(<ProductPreview product={curr} />);
+                  }
+                  return acc;
+                },
+                [[]]
+              )
+              .map((productPreviews, i) => {
+                return (
+                  <div key={i} className={styles.product_slide}>
+                    {productPreviews}
+                  </div>
+                );
+              })}
+          </animated.div>
         </div>
         <button type="button" onClick={pageUp}>
           <ArrowSvg dir="right" />
